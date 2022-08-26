5641dc1b5bc7e682d040044e088f2b871b769fe6
Sean Overton <sean14o8.so@gmail.com>
2022-08-26 13:52:38 +1000

    Started basic zombie

---
+++ Assets/Zombies/ZombieHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieHealth : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet"){
+            // for now, just default -10 damage 
+            health-=10;
+            Debug.Log(string.Format("Zombie shot: {0}", health));
+
+            // trigger animation saying "-10"
+        }
+    }    
+}
